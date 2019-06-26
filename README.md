afe94d22f26f1cc86980208adefc504ddc20ec53
Yannick Weiss <contact@yannickweiss.com>
2019-06-26 09:55:17 +0200

    using ip from argument

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # Siemens TD-3511 reader
 
+Deploy to raspberry pi: `./deploy.sh 192.168.3.a58`
 
+## Notes
+Protocol notes
+
+```
+/     ?      !    \r    \n
+[]byte("/?!\r\n")
+
+ACK    0      5   0    \r    \n
+[]byte("\x06060\r\n")
+```
 
 
 ## Material and Links
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+set -e
+set -x
+ARG1=${1:-"energy.local"}
+# compile for Raspberry Pi 3
 env GOOS=linux GOARCH=arm GOARM=6 go build -o read-values
-scp read-values pi@energy.local:~
-ssh pi@energy.local './read-values'
+scp read-values "pi@${ARG1}":~
+ssh "pi@${ARG1}" "~/read-values"
